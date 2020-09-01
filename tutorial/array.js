c09f1d1d4d0ead284d219b03982ab7cb2a9771d4
Ju minhark <wnalsgkr@gmail.com>
2020-09-01 13:46:05 +0900

    Update array.js

--- tutorial/array.js
+++ tutorial/array.js
@@ -2,13 +2,8 @@
 
 const str = 'The quick brown fox jumps over the lazy dog.';
 
-console.log(str.split(' ')[3]);
-// expected output: "fox"
-
-console.log(str.split(''));
-// expected output: "k"
-
-console.log(str.split());
-// expected output: Array ["The quick brown fox jumps over the lazy dog."]
+console.log(str.split(' ')[3]); // "fox"
+console.log(str.split('')[8]); //"k"
+console.log(str.split()); // ["The quick brown fox jumps over the lazy dog."]
 
 // TODO: array.slice()
