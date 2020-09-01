545d15e524a6da26b01c164da6494700201b17b2
Ju minhark <wnalsgkr@gmail.com>
2020-09-01 14:32:12 +0900

    Update array.js

--- tutorial/array.js
+++ tutorial/array.js
@@ -1,9 +1,10 @@
-// TODO: str.split([separator[, limit]])
+//* str.split([separator[, limit]])
 
 const str = 'The quick brown fox jumps over the lazy dog.';
 
 console.log(str.split(' ')[3]); // "fox"
-console.log(str.split('')[8]); //"k"
+console.log(str.split('')[8]); // "k"
+console.log(str.split('', 5)); // [ 'T', 'h', 'e', ' ', 'q' ]
 console.log(str.split()); // ["The quick brown fox jumps over the lazy dog."]
 
 // TODO: array.slice()
