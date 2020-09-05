234f93fed0b159e5d4919fa728cd381a2c980119
Ju minhark <wnalsgkr@gmail.com>
2020-09-05 12:05:32 +0900

    Update array.js

--- tutorial/array.js
+++ tutorial/array.js
@@ -18,6 +18,23 @@ arr = [0, 77, 33, 11, 99, 88, 55];
 console.log(Math.max.apply(null, arr)); // 99
 console.log(Math.min.apply(null, arr)); // 0
 
+//* str.split([separator[, limit]])
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.split('')[8]); // "k"
+console.log(str.split('', 5)); // [ 'T', 'h', 'e', ' ', 'q' ]
+console.log(str.split()); // ["The quick brown fox jumps over the lazy dog."]
+
+//* array.slice()
+
+//* arr.join() , arr.toString() : Array 합치기
+
+let arr = ['aaa', 'bbb', 'ccc', 'ddd'];
+
+console.log(arr.toString());
+console.log(arr.join());
+
 //* for in : key
 
 const obj = { a: 1, b: 2, c: 3 };
@@ -33,13 +50,3 @@ arr = [1, 2, 3];
 for (const v of arr) {
 	console.log(v);
 }
-
-//* str.split([separator[, limit]])
-
-const str = 'The quick brown fox jumps over the lazy dog.';
-
-console.log(str.split('')[8]); // "k"
-console.log(str.split('', 5)); // [ 'T', 'h', 'e', ' ', 'q' ]
-console.log(str.split()); // ["The quick brown fox jumps over the lazy dog."]
-
-//* array.slice()
