676f807e03e5b499e7264362554c9bfc78135f4c
Ju minhark <wnalsgkr@gmail.com>
2020-10-14 18:13:12 +0900

    Create math.js

---
+++ tutorial/math.js
@@ -0,0 +1,14 @@
+// 절대값 : Math.abs(매개변수)
+console.log(Math.abs(-2)); // 2
+
+// 거듭제곱 : Math.pow([대상 숫자], [거듭제곱 횟수]);
+console.log(Math.pow(2, 2)); // 4
+
+// 제곱근 : Math.sqrt([대상 숫자]);
+console.log(Math.sqrt(4)); // 2
+
+//! 빗변 계산
+console.log(Math.hypot(3, 4)); // 5
+console.log(Math.hypot(2, 2)); // 2.8284271247461903
+console.log(2 * Math.sqrt(2)); // 2.8284271247461903
+console.log(2 * Math.SQRT2); // 2.8284271247461903
