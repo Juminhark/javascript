4a1643486974461cc005f1242f139d8bc32370dc
Ju minhark <wnalsgkr@gmail.com>
2020-11-22 02:36:06 +0900

    Update script.js

--- Vanilla_JS_projects/github-profiles/script.js
+++ Vanilla_JS_projects/github-profiles/script.js
@@ -4,7 +4,25 @@ const getUser = async (user) => {
 	const resp = await fetch(APIURL + user);
 	const respData = await resp.json();
 
-	console.log(respData);
+	createUserCard(respData);
 };
 
-getUser('Juminhark');
+const createUserCard = (user) => {
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	card.innerHTML = `
+    <div>
+      <img src='${user.avatar_url}' alt='${user.name}' />
+    </div>
+    <div>
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers}</li>
+        <li>${user.following}</li>
+        <li>${user.public_repos}</li>
+      </ul>
+    </div>
+  `;
+};
