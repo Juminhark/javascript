3ddb98a672ef5d99994017d4bc9c0feeacf5ffdd
Ju minhark <wnalsgkr@gmail.com>
2021-04-16 17:03:18 +0900

    test

---
+++ test/script.js
@@ -0,0 +1,7 @@
+const test_container = document.querySelector('.test-container');
+
+const addText = document.createElement('div');
+
+addText.innerText = 'Hello World!';
+
+test_container.appendChild(addText);
