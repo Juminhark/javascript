acc799d63acf801d0c3fecb0c8414903a2d03bf3
Ju minhark <wnalsgkr@gmail.com>
2021-04-20 15:49:04 +0900

    minesweeper

---
+++ vanilla_Js/minesweeper/mineseeper.js
@@ -0,0 +1,20 @@
+// Logic
+
+export function createBoard(boardSize, numberOfMunes) {
+	const board = [];
+
+	for (let x = 0; x < boardSize; x++) {
+		const row = [];
+		for (let y = 0; y < boardSize; y++) {
+			const tile = {
+				x,
+				y,
+			};
+
+			row.push(tile);
+		}
+		board.push(row);
+	}
+
+	return board;
+}
---
+++ vanilla_Js/minesweeper/script.js
@@ -0,0 +1,15 @@
+// Display / UI
+
+import { createBoard } from './mineseeper';
+
+console.log(createBoard(2, 2));
+
+// 1. Populate a board with tiles/mines
+
+// 2. Left click on tiles
+// a. Reveal tiles
+
+// 3. Right click on tiles
+// a. Mark tiles
+
+// 4. Check for win/lose
