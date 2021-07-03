d94bd08118c301eefb064c23ae7351dd3d9e9c1a
Ju minhark <wnalsgkr@gmail.com>
2021-07-03 15:46:59 +0900

    nuggets

--- basic_nuggets/dynamic_object_keys/dynamic_object_keys.js
+++ basic_nuggets/dynamic_object_keys/dynamic_object_keys.js
@@ -2,14 +2,52 @@
 const person = {
 	name: 'john',
 };
-console.log(person.name); // john
+// console.log(person.name); // john
 
 person.age = 25;
-console.log(person); //{ name: 'john', age: 25 }
+// console.log(person); // { name: 'john', age: 25 }
 
 //? square bracket nation
 const items = {
 	'featured-items': ['item1', 'item2'],
 };
 
-console.log(items['featured-items']);
+// console.log(items['featured-items']); // [ 'item1', 'item2' ]
+// console.log(person['name']); // john
+
+let appState = 'loading';
+const keyName = 'computer';
+
+const app = {
+	[appState]: true,
+};
+
+app[keyName] = 'apple';
+
+// console.log(app); // { loading: true, computer: 'apple' }
+
+const state = {
+	loading: true,
+	name: '',
+	job: '',
+};
+
+function updateState(key, value) {
+	state[key] = value;
+}
+
+updateState('name', 'john');
+updateState('job', 'developer');
+updateState('loading', false);
+updateState('products', []);
+
+state.products.push('javascript');
+state['products'].push('react');
+
+console.log(state);
+// {
+//   loading: false,
+//   name: 'john',
+//   job: 'developer',
+//   products: [ 'javascript', 'react' ]
+// }
