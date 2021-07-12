b46a37dc27e91e3fe7afda4527833b605236f248
Ju minhark <wnalsgkr@gmail.com>
2021-07-12 22:42:48 +0900

    Update rest_operator.js

--- basic_nuggets/rest_operator/rest_operator.js
+++ basic_nuggets/rest_operator/rest_operator.js
@@ -8,18 +8,36 @@
 const fruits = ['apple', 'orange', 'lemon', 'banana', 'pear'];
 const [first, second, ...restOfArray] = fruits;
 
-console.log(first, second); // apple orange
-console.log(restOfArray); // [ 'lemon', 'banana', 'pear' ]
+// console.log(first, second); // apple orange
+// console.log(restOfArray); // [ 'lemon', 'banana', 'pear' ]
 
 const lemon = restOfArray.find((fruit) => fruit === 'lemon');
-console.log(lemon); // lemon
+// console.log(lemon); // lemon
+
+const copy = [...fruits];
+// console.log(copy === fruits);
+// console.log(fruits); //! 원본 영향 x
 
 //* objects
 const person = { name: 'john', lastName: 'smith', job: 'developer' };
 const { job, ...restOfObject } = person;
-console.log(job, restOfObject); // developer { name: 'john', lastName: 'smith' }
+// console.log(job, restOfObject); // developer { name: 'john', lastName: 'smith' }
+
+// console.log(person); //! 원본 영향 x
 
 //* functions
+const getAverage = (name, ...scores) => {
+	console.log(name); // john
 
-const copy = [...fruits];
-console.log(copy);
+	console.log(scores); // [ 89, 79, 21, 100 ]
+	const average =
+		scores.reduce((total, item) => {
+			return (total += item);
+		}, 0) / scores.length;
+	console.log(average);
+};
+
+getAverage(person.name, 89, 79, 21, 100);
+
+const testScores = [23, 45, 67, 89];
+getAverage('jain', ...testScores); //? gathering. collecting : 배열의 item을 풀어서
