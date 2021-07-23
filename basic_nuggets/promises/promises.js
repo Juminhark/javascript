6f22507aa98fb14a21833066b8f87c7133a91e36
Ju minhark <wnalsgkr@gmail.com>
2021-07-23 17:30:34 +0900

    nuggets

---
+++ basic_nuggets/promises/promises.js
@@ -0,0 +1,17 @@
+// Javascript Nuggets - Promises
+
+// async / await
+// consume / use promises
+
+// Pending, Rejected, FulFilled
+
+const add = (a, b) => {
+	console.log(a + b);
+	return a + b;
+};
+
+add(1, 2);
+
+const body = document.querySelector('body');
+
+body.style.background = 'green';
