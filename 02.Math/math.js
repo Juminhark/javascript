28b48a5f5086217aeb87c9a38f570d9f7272f862
Juminhark <40381241+Juminhark@users.noreply.github.com>
2022-09-13 18:14:20 +0900

    method

--- 02.Math/math.js
+++ 02.Math/math.js
@@ -1,10 +1,10 @@
-// 절대값 : Math.abs(매개변수)
+//? Math.abs() : 절대값 :
 console.log(Math.abs(-2)); // 2
 
-// 거듭제곱 : Math.pow([대상 숫자], [거듭제곱 횟수]);
+//? Math.pow([대상 숫자], [거듭제곱 횟수]) : 거듭제곱
 console.log(Math.pow(2, 2)); // 4
 
-// 제곱근 : Math.sqrt([대상 숫자]);
+//? Math.sqrt([대상 숫자]) : 제곱근
 console.log(Math.sqrt(4)); // 2
 
 //! 빗변 계산
@@ -12,3 +12,10 @@ console.log(Math.hypot(3, 4)); // 5
 console.log(Math.hypot(2, 2)); // 2.8284271247461903
 console.log(2 * Math.sqrt(2)); // 2.8284271247461903
 console.log(2 * Math.SQRT2); // 2.8284271247461903
+
+//? Math.ceil() : 올림
+console.log(Math.ceil(7.04)); // 8
+
+//? Math.floor() : 내림
+
+//? Math.round() : 반올림
