ef7fe4921b1d3a30453eb397bd41c8b6fadbde5e
Juminhark <40381241+Juminhark@users.noreply.github.com>
2023-10-10 18:06:59 +0900

    summary

--- mdn/Statements_declarations/for...of/README.md
+++ mdn/Statements_declarations/for...of/README.md
@@ -12,3 +12,29 @@ for (variable of iterable) {
 
 `variable` : 각 반복에 서로 다른 속성값이 variable에 할당
 `iterable` : 반복되는 열거가능(enumerable)한 속성이 있는 객체.
+
+## `for...of`와 `for...in`의 차이
+
+for...in 루프는 객체의 모든 열거가능한 속성에 대해 반복
+
+for...of 구문은 컬렉션 전용. 모든 객체보다는, [Symbol.iterator] 속성이 있는 모든 컬렉션 요소에 대해 이 방식으로 반복
+
+```js
+Object.prototype.objCustom = function () {};
+Array.prototype.arrCustom = function () {};
+
+let iterable = [3, 5, 7];
+iterable.foo = 'hello';
+
+for (let i in iterable) {
+  console.log(i); // logs 0, 1, 2, "foo", "arrCustom", "objCustom"
+}
+
+for (let i of iterable) {
+  console.log(i); // logs 3, 5, 7
+}
+```
+
+## reference
+
+- [mdn - for...of](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Statements/for...of)
--- mdn/Statements_declarations/for...of/index.js
+++ mdn/Statements_declarations/for...of/index.js
@@ -27,3 +27,55 @@ for (const value of iterable) {
 // 10
 // 20
 // 30
+
+//* `Map` 에 대해 반복
+iterable = new Map([
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+]);
+
+for (let entry of iterable) {
+  console.log(entry);
+}
+
+// [ 'a', 1 ]
+// [ 'b', 2 ]
+// [ 'c', 3 ]
+
+for (let [key, value] of iterable) {
+  console.log(key, value);
+}
+
+// a 1
+// b 2
+// c 3
+
+//* for...in / for...of 차이
+
+Object.prototype.objCustom = function () {};
+Array.prototype.arrCustom = function () {};
+
+iterable = [3, 5, 7];
+iterable.foo = 'hello';
+
+console.log(iterable);
+
+for (let i in iterable) {
+  console.log(i);
+}
+
+// 0
+// 1
+// 2
+// "foo"
+// "arrCustom"
+// "objCustom"
+
+for (let i of iterable) {
+  console.log(i);
+}
+
+// 3
+// 5
+// 7
